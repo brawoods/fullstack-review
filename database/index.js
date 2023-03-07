3e847bcba60247fba82661ff83617d199d19114d
Brandon Woods <woods.brandon.m@gmail.com>
2023-03-07 11:48:09 -0800

    connect database to server

--- database/index.js
+++ database/index.js
@@ -1,8 +1,28 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
-let repoSchema = mongoose.Schema({
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('connected');
+});
+
+let repoSchema = new mongoose.Schema({
   // TODO: your schema here!
+  "id": Number,
+  "name": String,
+  "full_name": String,
+  "html_url": String,
+  "description": String,
+  "url": String,
+  "size": Number,
+  "forks_count": Number,
+  "open_issues_count": Number,
+  "forks": Number,
+  "open_issues": Number,
+  "watchers": Number,
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
@@ -11,6 +31,9 @@ let save = (/* TODO */) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
+  // connect to app.post call after API
+  // create a new repo based on Repo model and API data
 }
 
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,32 @@
 const express = require('express');
+const db = require('../database');
+const save = require('../database/index.js');
 let app = express();
+
 
 // TODO - your code here!
 // Set up static file service for files in the `client/dist` directory.
 // Webpack is configured to generate files in that directory and
 // this server must serve those files when requested.
+app.use(express.static(`client/dist`)) //
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+
+  // ingest HTTP POST request from client
+  // GET from GitHub API (see helpers/github.js)
+  // save to db (see save from database/index.js)
+
 });
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+
+
 });
 
 let port = 1128;
